bd31547bb900bed6fc9e167e6e80bf3b5925778a
Ady <polcsek.dm90@gmail.com>
2022-01-18 16:18:38 +0100

    chore: refactor to oop + add ADDIT property

--- src/server/request-listener.js
+++ src/server/request-listener.js
@@ -1,50 +1,81 @@
 const router = require('./routers/todo.router');
 
-const UrlParser = require('./common/url-parser')
+const UrlParser = require('./common/url-parser');
 const BodyParser = require('./common/body-parser');
-const headerAppender = require('./common/header-appender');
+const ErrorParser = require('./common/error-parser');
+const HeaderAppender = require('./common/header-appender');
 
-module.exports = async (request, response) => {
-    try {
-        const { url, method } = request;
+class RequestListener {
+    #urlParser;
+    #bodyParser;
+    #headerAppender;
+    #errorParser;
 
-        const parsedUrl = UrlParser.parse(url)
-
-        const body = await BodyParser.parse(request)
-
-        const payload = {};
-
-        if (parsedUrl.path.id) {
-            payload.id = parsedUrl.path.id;
-        }
-
-        if (body) {
-            payload.body = body;
-        }
-
-        const func = router.use(method, url);
-
-        const { type, length, data } = await func(payload);
-
-        headerAppender(response, {
-            type,
-            length,
-        });
-
-        response.write(data);
-
-        return response.end();
-    } catch (error) {
-        console.log(error)
-        const { message, statusCode } = error;
-
-        headerAppender(response, {
-            type: 'error',
-            length: message.length,
-        });
-
-        response.write(message);
-
-        return response.end();
+    constructor(urlParser, bodyParser, errorParser, headerAppender) {
+        this.#urlParser = urlParser;
+        this.#bodyParser = bodyParser;
+        this.#headerAppender = headerAppender;
+        this.#errorParser = errorParser;
     }
-};
+
+    listen() {
+        return async (request, response) => {
+            try {
+                const { url, method } = request;
+
+                const parsedUrl = this.#urlParser.parse(url);
+
+                const body = await this.#bodyParser.parse(request);
+
+                const payload = {};
+
+                if (parsedUrl.path.id) {
+                    payload.id = parsedUrl.path.id;
+                }
+
+                if (body) {
+                    payload.body = body;
+                }
+
+                const func = router.use(method, parsedUrl.path);
+
+                const { type, length, data } = await func(payload);
+
+                this.#headerAppender(
+                    response,
+                    {
+                        headerType: type,
+                        length,
+                    },
+                    200
+                );
+
+                response.write(data);
+
+                return response.end();
+            } catch (error) {
+                const { message, statusCode } = this.#errorParser.parse(error);
+
+                this.#headerAppender(
+                    response,
+                    {
+                        headerType: 'error',
+                        length: message.length,
+                    },
+                    statusCode
+                );
+
+                response.write(message);
+
+                return response.end();
+            }
+        };
+    }
+}
+
+module.exports = new RequestListener(
+    UrlParser,
+    BodyParser,
+    ErrorParser,
+    HeaderAppender
+);
