491f827bd32ef4d128b367293598adb00f2f0b07
Ady <polcsek.dm90@gmail.com>
2022-01-18 16:20:08 +0100

    chore: simplify & refactor code base

--- src/server/common/header-appender.js
+++ src/server/common/header-appender.js
@@ -1,28 +1,24 @@
-const responseCodes = {
-    error: 404,
-    success: 200,
-};
-
 const headerOptions = {
     error: {
         'content-type': 'application/json',
-        'content-length': 0,
+        'content-length': null,
+        'access-control-allow-origin': '*',
+        'access-control-allow-methods': ['GET', 'POST', 'PUT', 'DELETE'],
     },
     success: {
         'content-type': 'application/json',
-        'content-length': 0,
+        'content-length': null,
+        'access-control-allow-origin': '*',
+        'access-control-allow-methods': ['GET', 'POST', 'PUT', 'DELETE'],
     },
 };
 
-module.exports = (response, options) => {
-    const { type, length } = options;
+module.exports = (response, options, code) => {
+    const { headerType, length } = options;
 
-    const code = responseCodes[type];
-    const header = headerOptions[type];
+    const header = headerOptions[headerType];
 
     header['content-length'] = length;
 
     response.writeHead(code, header);
-
-    return;
 };
