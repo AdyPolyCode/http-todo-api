a71815e6221175f74d40f55e2adee575723c2193
Ady <polcsek.dm90@gmail.com>
2022-01-18 16:17:53 +0100

    chore: refactor

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const NodeServer = require('./server/server');
-const requestListener = require('./server/request-listener');
+const RequestListener = require('./server/request-listener');
 const Config = require('./config/config-loader');
 
 const connectDB = require('./server/db/connect');
@@ -7,7 +7,7 @@ const connectDB = require('./server/db/connect');
 async function start() {
     await connectDB();
 
-    const server = new NodeServer(requestListener, Config);
+    const server = new NodeServer(RequestListener.listen(), Config);
 
     server.start();
 }
