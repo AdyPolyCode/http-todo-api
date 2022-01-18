e6c962260f41e4eaf7451575406e8c11562ef6d7
Ady <polcsek.dm90@gmail.com>
2022-01-18 16:20:33 +0100

    chore: refactor code base to oop

--- src/server/common/body-parser.js
+++ src/server/common/body-parser.js
@@ -1,27 +1,27 @@
-const Parser = require('./parser')
+const Parser = require('./parser');
 
 class BodyParser extends Parser {
-    static create(){
-        return new BodyParser()
+    static create() {
+        return new BodyParser();
     }
 
-    parse(request){
-        super.parse(request)
+    parse(request) {
+        super.parse(request);
 
         return new Promise((resolve, reject) => {
             request.setEncoding('utf8');
-    
+
             let body = '';
-    
+
             request
                 .on('data', (chunk) => {
                     body += chunk;
                 })
                 .on('end', () => {
-                    if(body){
+                    if (body) {
                         body = JSON.parse(body);
                     }
-    
+
                     resolve(body);
                 });
         });
